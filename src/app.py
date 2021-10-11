5fabfa20fd821c03cfa46fe744121faa0373d8b3
dj.kim <dj.kim@toss.im>
2021-10-11 18:45:49 +0900

    Delete hello api

--- src/app.py
+++ src/app.py
@@ -3,14 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-  return {
-    "status": 200,
-    "message": "hello world"
-  }
-
-
 @app.route("/healthz")
 def health():
   return {
