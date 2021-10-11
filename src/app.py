0f263137f0258eab07959e02c64dc72fdd0ca344
dj.kim <dj.kim@toss.im>
2021-10-11 19:41:47 +0900

    Add hello Api

--- src/app.py
+++ src/app.py
@@ -3,6 +3,14 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route("/hello")
+def hello():
+  return {
+    "status": 200,
+    "message": "hello world"
+  }
+
+
 @app.route("/healthz")
 def healthz():
   return {
