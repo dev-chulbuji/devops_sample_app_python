a0252b078fc0c712068f0b86040a8b52e758a79e
dj.kim <dj.kim@toss.im>
2021-10-12 21:46:53 +0900

    Add pytest

--- src/app.py
+++ src/app.py
@@ -1,23 +1,31 @@
-from flask import Flask
-
-app = Flask(__name__)
+from flask import Flask, jsonify
 
 
-@app.route("/healthz")
-def healthz():
-  return {
-    "status": 200,
-    "message": "healthy"
-  }
+def create_app():
+  app = Flask(__name__)
 
+  @app.route("/healthz")
+  def healthz():
+    return {
+      "status": 200,
+      "message": "healthy"
+    }
 
-@app.route("/hello")
-def hello():
-  return {
-    "status": 200,
-    "message": "hello world!!!"
-  }
+  @app.route("/hello")
+  def hello():
+    return jsonify({
+      "message": "Hello world\n"
+    }), 200
+
+  @app.route("/hello/<username>")
+  def hello_user(username):
+    return jsonify({
+      "message": "Hello %s\n" % username
+    }), 200
+
+  return app
 
 
 if __name__ == "__main__":
+  app = create_app()
   app.run(host='0.0.0.0')
---
+++ src/test_app.py
@@ -0,0 +1,27 @@
+import pytest
+
+from app import create_app
+
+
+@pytest.fixture(scope='session')  # 테스트 실행시 한번만 실행
+def app():
+  app = create_app()
+  return app
+
+
+@pytest.fixture
+def client(app):
+  client = app.test_client()
+  return client
+
+
+def test_hello(client):
+  rtv = client.get('/hello')
+  assert rtv.status_code == 200
+  assert rtv.json == {"message": "Hello world\n"}
+
+
+def test_hello_name(client):
+  rtv = client.get('/hello/dj')
+  assert rtv.status_code == 200
+  assert rtv.json == {"message": "Hello dj\n"}
