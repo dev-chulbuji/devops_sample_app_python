c6f7e27e75af34b88290c5c4fbdbcdbe6c7b5453
dj.kim <dj.kim@toss.im>
2021-11-06 18:38:42 +0900

    wip

--- deploy/scripts/run_process.sh
+++ deploy/scripts/run_process.sh
@@ -3,5 +3,5 @@
 ECR_DOCKER_IMAGE="657976307134.dkr.ecr.ap-northeast-2.amazonaws.com/demo"
 ECR_DOCKER_TAG="1.0.0"
 
-export IMAGE=$(ECR_DOCKER_IMAGE) && export TAG=$(ECR_DOCKER_TAG)
+export IMAGE=${ECR_DOCKER_IMAGE} && export TAG=${ECR_DOCKER_TAG}
 docker-compose -f docker-compose.yml up -d
