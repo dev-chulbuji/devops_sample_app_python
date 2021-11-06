ce095e1d6ab1282eff12f2f74bdea2245fcfd244
dj.kim <dj.kim@toss.im>
2021-11-07 02:57:22 +0900

    wip

--- deploy/scripts/run_process.sh
+++ deploy/scripts/run_process.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-ECR_DOCKER_IMAGE="657976307134.dkr.ecr.ap-northeast-2.amazonaws.com/demo"
+REGION="ap-northeast-2"
+ACCOUNT_ID="657976307134"
+ECR_REPOSITORY="${ACCOUNT_ID}.dkr.ecr.ap-northeast-2.amazonaws.com"
+ECR_DOCKER_IMAGE="${ECR_REPOSITORY}/demo"
 ECR_DOCKER_TAG="1.0.0"
+
+aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ECR_REPOSITORY};
 
 export IMAGE=${ECR_DOCKER_IMAGE};
 export TAG=${ECR_DOCKER_TAG};
