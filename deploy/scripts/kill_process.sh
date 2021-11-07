b8b55b82545b749133f8dba41d928d2ef76ffeca
dj.kim <dj.kim@toss.im>
2021-11-07 17:54:36 +0900

    wip

--- deploy/scripts/kill_process.sh
+++ deploy/scripts/kill_process.sh
@@ -2,3 +2,5 @@
 
 echo "Remove existed container"
 docker-compose -f /home/ec2-user/docker-compose.yml down || true
+
+
--- deploy/scripts/run_process.sh
+++ deploy/scripts/run_process.sh
@@ -6,8 +6,11 @@ ECR_REPOSITORY="${ACCOUNT_ID}.dkr.ecr.ap-northeast-2.amazonaws.com"
 ECR_DOCKER_IMAGE="${ECR_REPOSITORY}/demo"
 ECR_DOCKER_TAG="1.0.0"
 
-aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ECR_REPOSITORY};
+aws ecr get-login-password --region ${REGION} \
+  | docker login --username AWS --password-stdin ${ECR_REPOSITORY};
 
 export IMAGE=${ECR_DOCKER_IMAGE};
 export TAG=${ECR_DOCKER_TAG};
 docker-compose -f /home/ec2-user/docker-compose.yml up -d;
+
+
